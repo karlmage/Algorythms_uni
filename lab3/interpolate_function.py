d15b40b62cd30711f5cc43f9ba93d6ebaf07f4ef
karlmage <karlmagegit@gmail.com>
2026-05-11 23:49:05 +0200

    Enhance interpolation

--- lab3/interpolate_function.py
+++ lab3/interpolate_function.py
@@ -1,11 +1,18 @@
 from find_dots import find_dots
 from interpolate_dot import interpolate_dot
 
-def interpolate_function(i: int, a: float, b: float, p):
+def interpolate_function(p, a: int, b: int, i = 10):
     dots = find_dots(i, a, b, p)
     interpolated_function = {}
 
-    for i in dots.keys():
+    x_vals = []
+    x = a
+    step = float((b - a) / (i * 4))  # Визначаємо шаг відображення функції, щоб було видно похибку
+    while (x <= b):
+        x_vals.append(x)
+        x += step
+
+    for i in x_vals:
         interpolated_function[i] = interpolate_dot(i, dots)
 
     return interpolated_function
